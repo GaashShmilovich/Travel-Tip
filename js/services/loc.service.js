b8e5beb443daff0a895b20af0a9f212f17f5150f
Gaash Shmilovich <gaash.shm@gmail.com>
2023-06-28 21:06:51 +0300

    some minor changes

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,34 +1,32 @@
-import { storageService } from "./async-storage.service.js"
+import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 
 const LOCS_KEY = 'locsDB'
 
 export const locService = {
-    getLocs,
-    remove,
-    save
+  getLocs,
+  remove,
+  save,
 }
 
-
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
 
 function getLocs() {
-    var locs = storageService.query(LOCS_KEY) || [
-        { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-        { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-    ]
-    return locs
+  var locs = storageService.query(LOCS_KEY) || [
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+  ]
+  return locs
 }
 
 function remove(loc) {
-    return storageService.remove(LOCS_KEY, loc)
+  return storageService.remove(LOCS_KEY, loc)
 }
 
 function save(loc) {
-    if(loc) return storageService.put(LOCS_KEY, loc)
-    else return storageService.post(LOCS_KEY, loc)
+  if (loc) return storageService.put(LOCS_KEY, loc)
+  else return storageService.post(LOCS_KEY, loc)
 }
-
