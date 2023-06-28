09014a963491efb5b0c410aef7d7694efccfebbd
Giora Marani <giora.marani@gmail.com>
2023-06-28 18:23:40 +0300

    css added

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,15 +1,26 @@
-import { storageService } from "./a-sync-storage.service"
+import { storageService } from "./async-storage.service.js"
+import { utilService} from './util.service.js'
+
+const LOCS_KEY = 'locsDB'
 
 export const locService = {
     getLocs
 }
 
+_createLocs()
+function  _createLocs(){
+    let locs = utilService.loadFromStorage(LOCS_KEY)
+    if(!locs || !locs.lenght){
+        locs  = [
+            { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+            { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+        ]
+    }
+    utilService.saveToStorage(LOCS_KEY,locs)
+}
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
 
 function getLocs() {
-    return query()
+    const locs = storageService.query(LOCS_KEY)
+    return locs
 }
