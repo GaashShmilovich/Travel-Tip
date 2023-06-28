119c69183655cf4bdf96ae0ccf24d386138fffc6
Gaash Shmilovich <gaash.shm@gmail.com>
2023-06-28 18:46:38 +0300

    new api key

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,4 +1,5 @@
-import { storageService } from "./a-sync-storage.service"
+
+import { storageService } from "./a-sync-storage.service.js"
 
 export const locService = {
     getLocs
@@ -6,7 +7,7 @@ export const locService = {
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
